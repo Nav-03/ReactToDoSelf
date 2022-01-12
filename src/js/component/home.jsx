bb050352edd2f87e5aaca3da43506126a0e054a7
Nav-03 <navil.gabrie@gmail.com>
2022-01-12 17:30:40 +0000

    initial commit and almost final for submission, needs styling

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from "react";
 import Header from "./Header.jsx";
 import ToDoList from "./ToDoList.jsx";
-import Data from "./Data.json";
 
 //create your first component
 const Home = () => {
-	const [toDoList, setToDoList] = useState(Data);
 	return (
 		<>
-			<Header />
-			<ToDoList toDoList={toDoList} />
+			<ToDoList />
 		</>
 	);
 };
