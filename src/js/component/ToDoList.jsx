fa41db5db55e3a7b0c227b0c4fd0f6635cda3504
Nav-03 <navil.gabrie@gmail.com>
2022-01-27 21:56:35 +0000

    added get with fetch

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -45,6 +45,27 @@ function TodoForm({ addTodo }) {
 //List of To-Do items with functionality sent to render on page
 
 function ToDoList() {
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
+		method: "GET",
+		body: JSON.stringify(todos),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((resp) => {
+			console.log(resp.ok); // will be true if the response is successfull
+			console.log(resp.status); // the status code = 200 or code = 400 etc.
+			console.log(resp.text()); // will try return the exact result as string
+			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+		})
+		.then((data) => {
+			//here is were your code should start after the fetch finishes
+			console.log(data); //this will print on the console the exact object received from the server
+		})
+		.catch((error) => {
+			//error handling
+			console.log(error);
+		});
 	const [todos, setTodos] = useState([
 		{ text: "Learn about React", isCompleted: false },
 		{ text: "Practice React", isCompleted: false },
