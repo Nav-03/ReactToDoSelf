363b4312b31249a156df84182adc7cb0eae62905
Nav-03 <navil.gabrie@gmail.com>
2022-01-28 20:38:39 +0000

    added get from server and started adding put

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //Read list of to dos and display
 
@@ -45,35 +45,43 @@ function TodoForm({ addTodo }) {
 //List of To-Do items with functionality sent to render on page
 
 function ToDoList() {
-	fetch("https://assets.breatheco.de/apis/fake/todos/user/alesanchezr", {
-		method: "GET",
-		body: JSON.stringify(todos),
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-		.then((resp) => {
-			console.log(resp.ok); // will be true if the response is successfull
-			console.log(resp.status); // the status code = 200 or code = 400 etc.
-			console.log(resp.text()); // will try return the exact result as string
-			return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
-		})
-		.then((data) => {
-			//here is were your code should start after the fetch finishes
-			console.log(data); //this will print on the console the exact object received from the server
-		})
-		.catch((error) => {
-			//error handling
-			console.log(error);
-		});
+
+
+
+	const getAllTodos = async function () {
+		const options = {
+			method: "GET",
+			body: JSON.stringify(null),
+			headers: { "content-type": "application/json" },
+		};
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/unicorn",
+			options
+		);
+		setList(await response.json());
+	};
+
+	useEffect(() => {
+		//code goes here
+		getAllTodos();
+	}, []);
+
+
 	const [todos, setTodos] = useState([
 		{ text: "Learn about React", isCompleted: false },
 		{ text: "Practice React", isCompleted: false },
 		{ text: "Have nervous breakdown", isCompleted: false },
 	]);
-	const addTodo = (text) => {
-		const newTodos = [...todos, { text }];
-		setTodos(newTodos);
+	const addTodo = async (newTodos) => {
+		const options = {
+			method: "PUT",
+			body: JSON.stringify(newTodos),
+			headers: { "content-type": "application/json" },
+		};
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/unicorn",
+			options
+		);
 	};
 
 	const completeTodo = (index) => {
