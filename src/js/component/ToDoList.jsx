641f4204d080d8eded0596e48b91af56757b5dfa
Nav-03 <navil.gabrie@gmail.com>
2022-02-08 13:22:53 +0000

    changed GET method again

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -71,20 +71,13 @@ function ToDoList() {
 				"Content-Type": "application/json",
 			},
 		})
-			.then((resp) => {
-				console.log(resp.ok); // will be true if the response is successfull
-				console.log(resp.status); // the status code = 200 or code = 400 etc.
-				console.log(resp.text()); // will try return the exact result as string
-				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			.then((response) => {
+				if (response.status == 200) return response.json();
 			})
-			.then((data) => {
-				//here is were your code should start after the fetch finishes
-				console.log(data); //this will print on the console the exact object received from the server
+			.then((response) => {
+				// successfully completed, you should call the addTodo function here.
 			})
-			.catch((error) => {
-				//error handling
-				console.log(error);
-			});
+			.catch((error) => console.log("Error fetching todos:", error));
 	};
 
 	const completeTodo = (index) => {
